2eadd35195d5a3e4e82558d2e7abe678c0213fd4
kevin li <95264502+Kevinli7673@users.noreply.github.com>
2026-08-05 00:25:38 -0400

    Add: remove-node-from-end-of-linked-list - submission-2

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-2.py
@@ -0,0 +1,47 @@
+"""
+This is removing a val from a linked list nth value from the end (not front)
+For ex: if n = 5, we remove the 5th val from the end
+
+Idea: We can iterate through the linked list first to find the length of it
+Then we do length - n and find which element to remove
+So when we get to the element before length - n, we make next point to the element
+afterward
+
+Issue: just realized that if we're gonna remove first element ts a problem
+The idea is good, but what can i do in the code to make sure that it works
+for every case, and not just for some
+
+2nd idea: prob better
+So the entire program is gonna be a while loop:
+    set a dummy to move around 
+    keep a counter
+
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        N = 0
+        cur = head
+        while cur:
+            N += 1
+            cur = cur.next
+
+        removeIndex = N - n
+        if removeIndex == 0:
+            return head.next
+
+        cur = head
+        for i in range(N - 1):
+            if (i + 1) == removeIndex:
+                cur.next = cur.next.next
+                break
+            cur = cur.next
+        return head
+
