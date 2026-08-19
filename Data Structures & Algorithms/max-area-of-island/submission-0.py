da9585e294e162dbfcfe90fd55c77da3af71b136
kevin li <95264502+Kevinli7673@users.noreply.github.com>
2026-08-18 20:43:35 -0400

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.py
@@ -0,0 +1,28 @@
+"""
+Like the previous questions, we can use either bfs or dfs
+However, we need a way to save the size of each island
+Maybe we can use a hashmap to store graph: size and everytime dfs is called we do dfs(r, c, size+1)
+and saving that to the hashmap
+"""
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(r, c):
+            if (r < 0 or r == ROWS or c < 0 or
+                c == COLS or grid[r][c] == 0 or
+                (r, c) in visit
+            ):
+                return 0
+            visit.add((r, c))
+            return (1 + dfs(r + 1, c) +
+                        dfs(r - 1, c) +
+                        dfs(r, c + 1) +
+                        dfs(r, c - 1))
+
+        area = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                area = max(area, dfs(r, c))
+        return area
